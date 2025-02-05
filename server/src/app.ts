3e6346fb9808b3c9c2b9b3892c9dc14cd79d87d5
karthikeyan G <santhoshkarthisk005@gmail.com>
2025-02-05 17:40:44 +0530

    chnages for get and post, update pending

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,35 +1,6 @@
-import { urlencoded } from "body-parser";
-const pool = require('./config/db');
-
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
-
-app.use(bodyParser,urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use(express.static('public'));
-
-
-const testQuery = async () => {
-    try {
-      const result = await pool.query('SELECT NOW()');
-      console.log('Database Time:', result.rows[0]);
-    } catch (err) {
-      console.error('Error executing query', err);
-    } finally {
-      pool.end(); // Close the connection
-    }
-  };
-  
-  testQuery();
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
-
-
-
+export default app;
